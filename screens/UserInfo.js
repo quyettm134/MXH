ae77c1aeda608baab0ead0c3f46b979907130171
pdat2k1 <ph.dat.2k1@gmail.com>
2022-12-15 22:42:17 +0700

    linh tinh

--- screens/UserInfo.js
+++ screens/UserInfo.js
@@ -4,89 +4,89 @@ import { FocusedStatusBar } from '../components';
 
 const UserInfo = () => {
 
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <FocusedStatusBar background={COLORS.primary}/>
+	return (
+		<SafeAreaView style={{ flex: 1 }}>
+			<FocusedStatusBar background={COLORS.primary} />
 
-            <View style={styles.container}>
-                <View style={styles.header}></View>
-                <Image style={styles.avatar} source={assets.person02}/>
-                <View style={styles.body}>
-                    <View style={styles.bodyContent}>
-                      <View style={styles.nameContainer}>
-                        <Text style={{ color: "#000000", fontWeight: "bold", fontSize: 32 }}>User</Text> 
-                      </View>  
-                                  
-                      <TouchableOpacity style={styles.buttonContainer}>
-                        <Text style={{ color: "#FFFFFF" }}>Add friend</Text> 
-                      </TouchableOpacity>
+			<View style={styles.container}>
+				<View style={styles.header}></View>
+				<Image style={styles.avatar} source={assets.person02} />
+				<View style={styles.body}>
+					<View style={styles.bodyContent}>
+						<View style={styles.nameContainer}>
+							<Text style={{ color: "#000000", fontWeight: "bold", fontSize: 32 }}>User</Text>
+						</View>
 
-                      <TouchableOpacity style={styles.buttonContainer}>
-                        <Text style={{ color: "#FFFFFF" }}>Call</Text> 
-                      </TouchableOpacity>
+						<TouchableOpacity style={styles.buttonContainer}>
+							<Text style={{ color: "#FFFFFF" }}>Add friend</Text>
+						</TouchableOpacity>
 
-                      <TouchableOpacity style={styles.buttonContainer}>
-                        <Text style={{ color: "#FFFFFF" }}>Message</Text> 
-                      </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-        </SafeAreaView>
-    )
+						<TouchableOpacity style={styles.buttonContainer}>
+							<Text style={{ color: "#FFFFFF" }}>Call</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity style={styles.buttonContainer}>
+							<Text style={{ color: "#FFFFFF" }}>Message</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
+			</View>
+		</SafeAreaView>
+	)
 }
 
 const styles = StyleSheet.create({
-    header: {
-        height: 200,
-        backgroundColor: COLORS.primary
-    },
-    backgroundImage: {
-        flex: 1,
-        justifyContent: "center"
-    },
-    avatar: {
-        width: 130,
-        height: 130,
-        borderRadius: 63,
-        borderWidth: 4,
-        borderColor: "white",
-        marginBottom: 10,
-        alignSelf: 'center',
-        position: 'absolute',
-        marginTop: 130
-    },
-    name: {
-      fontSize: 22,
-      color: "#FFFFFF",
-      fontWeight: '600',
-    },
-    body: {
-      marginTop: 40,
-    },
-    bodyContent: {
-      flex: 1,
-      alignItems: 'center',
-      padding: 30,
-    },
-    nameContainer: {
-      marginTop: 10,
-      height: 45,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginBottom: 10,
-    },
-    buttonContainer: {
-      marginTop: 20,
-      height: 45,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginBottom: 20,
-      width: 250,
-      borderRadius: 30,
-      backgroundColor: COLORS.primary
-    },
+	header: {
+		height: 200,
+		backgroundColor: COLORS.primary
+	},
+	backgroundImage: {
+		flex: 1,
+		justifyContent: "center"
+	},
+	avatar: {
+		width: 130,
+		height: 130,
+		borderRadius: 63,
+		borderWidth: 4,
+		borderColor: "white",
+		marginBottom: 10,
+		alignSelf: 'center',
+		position: 'absolute',
+		marginTop: 130
+	},
+	name: {
+		fontSize: 22,
+		color: "#FFFFFF",
+		fontWeight: '600',
+	},
+	body: {
+		marginTop: 40,
+	},
+	bodyContent: {
+		flex: 1,
+		alignItems: 'center',
+		padding: 30,
+	},
+	nameContainer: {
+		marginTop: 10,
+		height: 45,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginBottom: 10,
+	},
+	buttonContainer: {
+		marginTop: 20,
+		height: 45,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginBottom: 20,
+		width: 250,
+		borderRadius: 30,
+		backgroundColor: COLORS.primary
+	},
 });
 
 export default UserInfo;
